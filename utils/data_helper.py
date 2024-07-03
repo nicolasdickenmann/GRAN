52d9eabd1f811beb4bec7801fa65377b1bdb02af
nicolasdickenmann <nicolasdickenmann@github.com>
2024-07-03 18:17:59 +0200

    dia3

--- utils/data_helper.py
+++ utils/data_helper.py
@@ -205,6 +205,14 @@ def create_graphs(graph_type, data_dir='data', noise=10.0, seed=1234):
         name='FIRSTMM_DB',
         node_attributes=False,
         graph_labels=True)
+  elif graph_type == 'dia3':
+    graphs = graph_load_batch(
+        data_dir,
+        min_num_nodes=0,
+        max_num_nodes=10000,
+        name='dia3',
+        node_attributes=False,
+        graph_labels=True)
 
   num_nodes = [gg.number_of_nodes() for gg in graphs]
   num_edges = [gg.number_of_edges() for gg in graphs]
